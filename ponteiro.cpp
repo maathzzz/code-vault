5050835a1766ff714b1311fbaae7220f94aa0944
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-04-05 19:53:49 -0300

    Update ponteiro.cpp

--- ponteiro.cpp
+++ ponteiro.cpp
@@ -3,14 +3,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	int valor = 110;
+    int valor = 110;
     int *ponteiro;
     
     ponteiro = &valor;
     
     printf("\n  Valor da variavel valor: %d", valor);
-    printf("\n Endereço da variavel valor: %d", &valor);
+    printf("\n EndereÃ§o da variavel valor: %d", &valor);
     
-    printf("\n Endereço da variavel ponteiro: %d", &ponteiro);
+    printf("\n EndereÃ§o da variavel ponteiro: %d", &ponteiro);
     printf("\n Valor da variavel ponteiro: %d", ponteiro);
 }
